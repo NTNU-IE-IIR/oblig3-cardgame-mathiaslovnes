98849153531c1586e9b6afc54c2b268482182034
Mathias Løvnes <mathiaslovnes02@gmail.com>
2024-02-28 11:14:23 +0100

    Added dealHand method in DeckOfCards

--- src/main/java/no/ntnu/idatx2003/oblig3/cardgame/DeckOfCards.java
+++ src/main/java/no/ntnu/idatx2003/oblig3/cardgame/DeckOfCards.java
@@ -2,20 +2,38 @@ package no.ntnu.idatx2003.oblig3.cardgame;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Random;
+import java.util.Random;
 
 public class DeckOfCards {
   private final char[] suits = {'S', 'H', 'D', 'C'};
-  private final char[] faces = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
-  private PlayingCard[] deck;
+  private final int[] faces = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
+  private ArrayList<PlayingCard> deck;
+  Random rand;
 
   public DeckOfCards() {
-    deck = new PlayingCard[52];
-    int i = 0;
+    deck = new ArrayList<>();
+    rand = new Random();
     for (char suit : suits) {
-      for (char face : faces) {
-        deck[i] = (new PlayingCard(suit, face));
-        i++;
+      for (int face : faces) {
+        deck.add(new PlayingCard(suit, face));
       }
     }
   }
+
+  public ArrayList<PlayingCard> dealHand(int n) {
+    if (n > deck.size()) {
+      throw new IllegalArgumentException("No more cards left in the deck");
+    }
+
+    ArrayList<PlayingCard> dealtHand = new ArrayList<>();
+    for (int i = 0 ; i < n ; i++) {
+      PlayingCard playingCard = deck.get(rand.nextInt(0, deck.size()));
+      dealtHand.add(playingCard);
+      deck.remove(playingCard);
+      System.out.println(playingCard.getAsString());
+    }
+    return dealtHand;
+  }
+
 }
--- src/main/java/no/ntnu/idatx2003/oblig3/cardgame/MainApp.java
+++ src/main/java/no/ntnu/idatx2003/oblig3/cardgame/MainApp.java
@@ -2,7 +2,10 @@ package no.ntnu.idatx2003.oblig3.cardgame;
 
 public class MainApp {
   public static void main(String[] args) {
-    Ui.appMain(args);
+    DeckOfCards deck = new DeckOfCards();
+    deck.dealHand(52);
+    deck.dealHand(1);
+
   }
 }
 
